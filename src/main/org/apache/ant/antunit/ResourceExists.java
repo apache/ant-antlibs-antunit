edae631d028527dcf1d58f7a1851dd51bcd326f1
Stefan Bodewig <bodewig@apache.org>
2012-06-17 07:10:05 +0000

    committed debugging line
    git-svn-id: https://svn.apache.org/repos/asf/ant/antlibs/antunit/trunk@1351064 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/org/apache/ant/antunit/ResourceExists.java
+++ src/main/org/apache/ant/antunit/ResourceExists.java
@@ -44,7 +44,6 @@ public class ResourceExists extends ProjectComponent implements Condition {
             throw new BuildException("Only one resource can be tested.");
         }
         resource = r;
-        System.err.println("R: " + r);
     }
 
     /**
